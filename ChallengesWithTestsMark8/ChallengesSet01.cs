d495277a947737ae29029573792f678921327d30
mangosax <mcguerty@att.net>
2023-09-04 20:40:03 -0500

    weekly challenges

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -18,32 +18,62 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            {
+
+                return minuend - subtrahend;
+
+            }
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 + number2;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1 < number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2;   
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            if (nameOfPerson == "John Thomas")
+            {
+                return "Hello, John Thomas!";
+            }
+            else if (nameOfPerson == "John")
+            {
+                return "Hello, John!";
+            }
+            else if (nameOfPerson == "Leigh Ann")
+            {
+                return "Hello, Leigh Ann!";
+            }
+            else if (nameOfPerson == "Leigh")
+            {
+                return "Hello, Leigh!";
+            }
+            else
+            {
+                return "Hello!";
+            }
         }
 
-        public string GetHey()
+            public string GetHey()
         {
-            throw new NotImplementedException();
+            return "HEY!";
         }
     }
 }
